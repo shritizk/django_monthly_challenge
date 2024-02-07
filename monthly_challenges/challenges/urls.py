4e0aae6dc7052bfd105fb2b20d32b7f81e8db9f7
shritizk <shritizk@gmail.com>
2024-02-08 00:24:41 +0530

    made dynamic django urls

---
+++ monthly_challenges/challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+  path("<int:month>",views.monthly_challenges_withnumber),
+  path("<str:month>",views.monthly_challenges,name="monthly_challenges")
+]
+
---
+++ monthly_challenges/challenges/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse ,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
+
+
+list1={"january":"Join gym",
+      "february":"Do cardio",
+      "march":"Study",
+      "april":"Run every day",
+      "may":"Swim",
+      "june":"train your dog",
+      "july":"get a job",
+      "auguest":"do steroids",
+      "september":"bench 100kg",
+      "octuber":"learn to drive",
+      "november":"get a new car",
+      "december":"go on holiday"}
+
+def monthly_challenges_withnumber(request,month):
+  months=list(list1.keys())
+
+  if month>len(months):return HttpResponseNotFound("Number excede available rage")
+
+  redirct=months[month-1]
+  redirct_url=reverse("monthly_challenges",args=[redirct])
+  return HttpResponseRedirect(redirct_url)
+
+def monthly_challenges(request,month):
+ try :
+  txt=list1[month]
+  return HttpResponse(txt)
+ except:
+  return HttpResponseNotFound("Month name is incorrect")
+  
+  
+  
+    
